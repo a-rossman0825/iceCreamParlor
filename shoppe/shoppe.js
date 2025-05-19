ae321a06297f4a64504d92c8ccc55dbac42771a7
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-05-19 13:22:07 -0600

    Add draw functions to JS

--- shoppe/shoppe.js
+++ shoppe/shoppe.js
@@ -1,4 +1,4 @@
-const items = [
+const menu = [
   {
     name: 'barbarian',
     price: 3.5,
@@ -61,9 +61,47 @@ const items = [
   },
 ];
 
-const shoppingCart = document.getElementById('cart-items');
+//DOM Variable Declarations
+let shoppingCartElm = document.getElementById('cart-items');
+console.log(shoppingCartElm);
+let cartTotalElm = document.getElementById('cart-total')
 
+//Add Item to Cart (increase quantity)
 function addToCart(itemName) {
-  console.log('addedItem', itemName);
+  console.log('ordering', itemName);
+  for (let i=0; i<menu.length; i++) {
+    const item = menu[i];
+    console.log(i, item);
+    if (itemName == item.name) {
+      item.quantity++;
+    }
+  }
+  drawToCart();
+  findTotal();
+};
 
+function drawToCart() {
+  let cartContent = '';
+  let total = 0;
+  for(let i=0; i<menu.length; i++) {
+    const item = menu[i];
+    if (item.quantity > 0) {
+      cartContent += `<p class="cart-content fw-bold medieval-font">${item.quantity}x ${item.name} $${(item.price * item.quantity).toFixed(2)}</p>`;
+    }
+  }
+  shoppingCartElm.innerHTML = cartContent;
+};
+
+
+function findTotal() {
+  let totalContent = '';
+  let total = 0;
+  for(let i=0; i<menu.length; i++) {
+    const item = menu[i];
+    if (item.quantity > 0) {
+      total += (item.price * item.quantity);
+      totalContent += `<p class="cart-content fw-bold medieval-font">Total: $${total}</p>`;
+    }
+  }
+  shoppingCartElm.innerHTML = totalContent;
 }
